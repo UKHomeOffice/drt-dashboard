21c808b612b1b70ac91d4e4e8ae9f9ed7da61867
Nilesh Gupta <nilesh.nemichandgupta@digital.homeoffice.gov.uk>
2022-08-09 13:47:42 +0100

    DRTII-1002 update region page fonts

--- react/src/components/RegionalPort.tsx
+++ react/src/components/RegionalPort.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import ArrivalExport from './ArrivalExport';
 import {UserProfile} from "../model/User";
 import {ConfigValues} from "../model/Config";
@@ -16,24 +14,18 @@ export const RegionalPort = (props: IProps) => {
     return <div class="flex-container">
         {props.user.roles.includes("rcc:" + props.region.toLowerCase()) ?
             <div>
-                <Box>
-                    <Typography align="left" variant="h6" component="h2">
-                        {props.region} region dashboard
-                    </Typography>
-                    <p> This is a new page on DRT. You can download an arrivals export covering all port terminals in
-                        this region.</p>
-                    <div align="left"><ArrivalExport region={props.region}/></div>
-                    <p> A member of the team will be in touch to get your thoughts about how this
-                        page could be more useful. You can also get in touch with the team by email at
-                        <a href={"mailto:" + props.config.teamEmail}> {props.config.teamEmail}</a>.</p>
-                </Box>
+                <h1> {props.region} region dashboard </h1>
+                <p> This is a new page on DRT. You can download an arrivals export covering all port terminals in
+                    this region.</p>
+                <div align="left"><ArrivalExport region={props.region}/></div>
+                <p> A member of the team will be in touch to get your thoughts about how this
+                    page could be more useful. You can also get in touch with the team by email at
+                    <a href={"mailto:" + props.config.teamEmail}> {props.config.teamEmail}</a>.</p>
                 <Button style={{float: 'right'}} href="/">back</Button>
             </div> :
             <div>
-                <Box>
-                    <p> You don't have access to this page. To request access please get in touch with the team at <a
-                        href={"mailto:" + props.config.teamEmail}> {props.config.teamEmail}</a>.</p>
-                </Box>
+                <p> You don't have access to this page. To request access please get in touch with the team at <a
+                    href={"mailto:" + props.config.teamEmail}> {props.config.teamEmail}</a>.</p>
             </div>
         }
     </div>
