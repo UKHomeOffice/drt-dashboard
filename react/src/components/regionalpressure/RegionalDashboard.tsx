a6dfa113188b92785bbe61552400699fb79fcff9
Jackson Hyde <jacksonjhyde@gmail.com>
2025-09-25 09:12:22 +0100

    Regional dashboard title fixes

--- react/src/components/regionalpressure/RegionalDashboard.tsx
+++ react/src/components/regionalpressure/RegionalDashboard.tsx
@@ -82,12 +82,16 @@ const RegionalDashboard = ({ config, forecastHourlyPaxByPort, historicHourlyPaxB
 
   return (
     <Box sx={{ p: 2 }}>
+      <Stack direction="row" spacing={2} alignItems="center" sx={{ mb: 2 }}>
+          <IconButton component={Link} to="/national-pressure" size='small'><ArrowBack /></IconButton>
+          <Typography variant='h2' sx={{ textTransform: 'capitalize', marginBottom: 0 }}>{ title }</Typography>
+      </Stack>
       <Grid container spacing={2} justifyItems={'stretch'} alignContent={'center'}>
         <Grid>
           <IconButton component={Link} to="/national-pressure" size='small' sx={{ margin: '16px 10px 0 16px' }}><ArrowBack /></IconButton>
         </Grid>
         <Grid>
-          <Typography variant='h2' style={{ textTransform: 'capitalize' }}>{ title }</Typography>
+          <Typography variant='h2' sx={{ textTransform: 'capitalize', marginBottom: 0 }}>{ title }</Typography>
         </Grid>
       </Grid>
       <Grid>
