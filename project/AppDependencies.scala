129ea99585cb3f04f4973d898fa6c4c578f0ac5c
slawekb <259381811+slawomirbedraHO@users.noreply.github.com>
2026-08-05 16:22:11 +0100

    DRTII-2232-2249 | drt-lib version alignment with drtv2.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   private val scalaTestVersion = "3.2.20"
 
   private val ciriumVersion = "v425"
-  private val drtLibVersion = "v1459"
+  private val drtLibVersion = "v1462"
 
   val compileDependencies: Seq[ModuleID] = Seq(
     "org.apache.pekko"           %% "pekko-actor-typed"         % pekkoVersion,
