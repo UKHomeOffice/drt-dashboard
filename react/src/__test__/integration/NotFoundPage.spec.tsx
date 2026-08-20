9988352bd6cfebcf4a50ac947af6b058fed433a4
Hamza <hamza.sabat@digital.homeoffice.gov.uk>
2026-08-20 18:43:40 +0100

    DRTII-1432: Added Unit Test to validate the content of the not found page

---
+++ react/src/__test__/integration/NotFoundPage.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFoundPage from '../../components/NotFoundPage';
+
+describe('NotFoundPage', () => {
+  it('renders heading, guidance text and team email link', () => {
+    render(<NotFoundPage />);
+
+    expect(screen.getByRole('heading', { name: /Page not found/i })).toBeInTheDocument();
+    expect(screen.getByText(/If you typed the web address, check it is correct\./i)).toBeInTheDocument();
+    expect(screen.getByText(/If you pasted the web address, check you copied the entire address\./i)).toBeInTheDocument();
+
+    const emailLink = screen.getByRole('link', { name: /drtpoiseteam@homeoffice.gov.uk/i });
+    expect(emailLink).toHaveAttribute('href', 'mailto:drtpoiseteam@homeoffice.gov.uk');
+  });
+});
