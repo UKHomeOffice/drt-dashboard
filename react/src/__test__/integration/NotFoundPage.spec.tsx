fafaccd32b61038897c136f5784acef745dbc718
Hamza <hamza.sabat@digital.homeoffice.gov.uk>
2026-08-21 11:59:33 +0100

    DRTII-1432: Updated broken unit test for page title

--- react/src/__test__/integration/NotFoundPage.spec.tsx
+++ react/src/__test__/integration/NotFoundPage.spec.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import NotFoundPage from '../../components/NotFoundPage';
 
 describe('NotFoundPage', () => {
-  it('renders heading, guidance text and team email link', () => {
+  it('renders heading, guidance text and team email link', async () => {
     render(<NotFoundPage />);
 
-    expect(document.title).toBe('Page not found - Dynamic Response Tool - Border Force');
+    await waitFor(() => {
+      expect(document.title).toBe('Page not found - Dynamic Response Tool - Border Force');
+    });
 
     expect(screen.getByRole('heading', { name: /Page not found/i })).toBeInTheDocument();
     expect(screen.getByText(/If you typed the web address, check it is correct\./i)).toBeInTheDocument();
