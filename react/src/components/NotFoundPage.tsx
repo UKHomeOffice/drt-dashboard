664ebebfc0a52b76b0efb37d7af1e6c058568525
Hamza <hamza.sabat@digital.homeoffice.gov.uk>
2026-08-21 10:34:54 +0100

    DRTII-1432: Added specific Page Title (helmet) according to spec defined in ticket for the Not Found Page

--- react/src/components/NotFoundPage.tsx
+++ react/src/components/NotFoundPage.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 
 const NotFoundPage = () => (
   <div className="govuk-width-container" data-testid="not-found-page">
+    <Helmet>
+      <title>Page not found - DRT</title>
+    </Helmet>
     <main
       className="govuk-main-wrapper govuk-main-wrapper--l"
       id="main-content"
